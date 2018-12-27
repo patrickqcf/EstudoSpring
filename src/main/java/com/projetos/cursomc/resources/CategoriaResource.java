08caf4e5e3f9e0ec0c9d990cb80996d31b80867b
Patrick Fernandez <patrickqcf@gmail.com>
2018-12-27 20:46:04 -0200

    Inicio do desenvolvimento das operações de CRUD e aplicações front end. Disponibilizado a inserção de novas categorias pelo POST e JSON

--- src/main/java/com/projetos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/projetos/cursomc/resources/CategoriaResource.java
@@ -1,12 +1,16 @@
 package com.projetos.cursomc.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetos.cursomc.domain.Categoria;
 import com.projetos.cursomc.services.CategoriaService;
@@ -23,4 +27,13 @@ public class CategoriaResource {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
 	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert (@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	
 }
--- src/main/java/com/projetos/cursomc/services/CategoriaService.java
+++ src/main/java/com/projetos/cursomc/services/CategoriaService.java
@@ -23,4 +23,9 @@ public class CategoriaService {
 				"Objeto não encontrado! ID: " + id + ", Tipo: " +Categoria.class.getName()));
 	}
 	
+	public Categoria insert (Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
