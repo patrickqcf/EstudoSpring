d57adc317271e18aa9fbca0f7d4ffdf14afe13ab
Patrick Fernandez <patrickqcf@gmail.com>
2019-01-05 22:35:25 -0200

    Listando todas as categorias com DTO

--- src/main/java/com/projetos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/projetos/cursomc/resources/CategoriaResource.java
@@ -2,6 +2,8 @@ package com.projetos.cursomc.resources;
 
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetos.cursomc.domain.Categoria;
+import com.projetos.cursomc.dto.CategoriaDTO;
 import com.projetos.cursomc.services.CategoriaService;
 
 @RestController
@@ -48,4 +51,12 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() { 
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok(listDto);
+	}
+	
 }
--- src/main/java/com/projetos/cursomc/services/CategoriaService.java
+++ src/main/java/com/projetos/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.projetos.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,6 +47,9 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados!");
 		}
 	}	
-		
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 	
 }
