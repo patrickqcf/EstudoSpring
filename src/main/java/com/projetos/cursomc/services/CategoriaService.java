4e7e9df02f7956e309735587d85af754076a7003
Patrick Fernandez <patrickqcf@gmail.com>
2019-01-08 14:26:57 -0200

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/projetos/cursomc/services/CategoriaService.java
+++ src/main/java/com/projetos/cursomc/services/CategoriaService.java
@@ -38,8 +38,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update (Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -63,6 +64,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 	
 }
