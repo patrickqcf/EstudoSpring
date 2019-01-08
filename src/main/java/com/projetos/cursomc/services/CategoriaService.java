5e3d8d16529326c3d0415918a1918e8d42a4874e
Patrick Fernandez <patrickqcf@gmail.com>
2019-01-08 11:13:54 -0200

    Validação sintática com Bean Validation

--- src/main/java/com/projetos/cursomc/services/CategoriaService.java
+++ src/main/java/com/projetos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.projetos.cursomc.domain.Categoria;
+import com.projetos.cursomc.dto.CategoriaDTO;
 import com.projetos.cursomc.repositories.CategoriaRepository;
 import com.projetos.cursomc.services.exceptions.DataIntegrityException;
 import com.projetos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,6 +59,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
